0b17fd46ac5a421b3fcff57510ad3f643dce3fed
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-06-24 14:28:42 +0530

    useref case 1 immmplemeted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useRef } from "react"
+
 
 function App() {
 
+  const ref=useRef();
+
+  function getfocous(){
+    ref.current.focus(); //it is like we are getting id by javascript way
+  }
+
   return (
-    <>
-    Hi there
-    </>
+    <div>
+      Signup
+      <input  ref={ref}type="text" />
+      <input type="text" />
+      <button onClick={getfocous}>Submit</button>
+   
+
+    </div>
   )
 }
 
